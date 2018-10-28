b21537d472e1d8226e5eb8f01f1b358299d11dc7
gouthami <mukkapallygouthamipriya@gmail.com>
2018-10-28 12:34:33 -0500

    added caluclator API's

---
+++ src/welcome/Calculator.java
@@ -0,0 +1,17 @@
+package welcome;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+int sum=add(10,20);
+System.out.println("sum="+sum);
+	}
+	public static int add(int a,int b) {
+	
+	return a+b;
+	}
+	
+
+}
